cf421314f89cc05d3b0c080b13eb1148656223fd
Bulseeye <50776735+Bulseeye@users.noreply.github.com>
2025-04-06 12:53:55 +0200

    Time: 0 ms (100%), Space: 21 MB (73.08%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,15 +7,31 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         # recursive solution is just a inorder traversal
-        arr = []
+        # arr = []
 
-        def inorder(root):
-            if not root:
-                return
+        # def inorder(root):
+        #     if not root:
+        #         return
 
-            inorder(root.left)
-            arr.append(root.val)
-            inorder(root.right)
-        inorder(root)
-        return arr[k-1]
+        #     inorder(root.left)
+        #     arr.append(root.val)
+        #     inorder(root.right)
+        # inorder(root)
+        # return arr[k-1]
+
+        # Iterative
+        stack = []
+        cur = root
+
+        # as long as we have a cur or a stack keep running the code.
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
           
