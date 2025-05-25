ed8e197e476baec2991df3f27cd932a1a7ad5482
Bulseeye <50776735+Bulseeye@users.noreply.github.com>
2025-05-25 23:58:23 +0200

    Time: 0 ms (100%), Space: 18 MB (13.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -9,9 +9,16 @@ class Solution:
             if i in cache:
                 return cache[i]
 
+            if dfs(i+1) > nums[i] + dfs(i + 2):
+                cache[i] = dfs(i + 1)
+            else:
+                cache[i] = nums[i] + dfs(i + 2)
+
             # loops 1, 3, 5 etc
             # then 0 + 2, 4 then return the highest count in the cache
-            cache[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            
+            # cache[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            
             return cache[i]
 
         return dfs(0)
