cd87f1836bc96036662f1caef214f17d24e6c374
Bulseeye <50776735+Bulseeye@users.noreply.github.com>
2025-06-12 11:00:51 +0200

    Time: 4 ms (68.47%), Space: 18.5 MB (59.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,9 +2,13 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         # create a bit array
         bitarr = [0] * (n + 1)
-        # create an offset
+        # create an offset and increase when i matches
         offset = 1
 
+        # count how many 1s are in the current number bit representation
+        #             8421
+        #              4 1 = 5
+        # example 5 = 0101 = 2 (1s in there)
         for i in range(1, n + 1):
             if offset * 2 == i:
                 offset = i
